b193e66a3305ca7dc40dd2f2807cc8245aa02ec5
XDancer <111144908+andylebeau@users.noreply.github.com>
2023-05-30 14:20:20 -0400

    Added 'change screen size' capability

--- scripts.js
+++ scripts.js
@@ -1,20 +1,44 @@
-const grid = document.getElementById('container');
+let gridSize = 16;
 
-function makeGrid() {
-    for (let rows = 0; rows < 16; rows++) {
-        for (let columns = 0; columns < 16; columns++) {
-            const row = document.createElement('div');
-            row.classList.add('pixel');
-            grid.appendChild(row);
-        };
+function makeGrid(size) {
+    const grid = document.getElementById('grid-container');
+    const widthAndHeight = 960 / size;
+    for (let i = 0; i < size**2; i++) {
+        const cell = document.createElement('div');
+        cell.setAttribute('id','pixel')
+        grid.appendChild(cell)
     };
+    const cellSize = document.querySelectorAll('#pixel')
+        cellSize.forEach((cell) => {
+            cell.style.width = `${widthAndHeight}px`;
+            cell.style.height = `${widthAndHeight}px`
+        });
+    const pen = document.querySelectorAll('#pixel');
+        pen.forEach((cell) => {
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = 'gray';
+            })
+        })    
 }
 
-makeGrid();
+makeGrid(gridSize);
 
-const pen = document.querySelectorAll('.pixel');
-    pen.forEach((cell) => {
-        cell.addEventListener('mouseover', () => {
-            cell.style.backgroundColor = 'gray';
-        })
+const changeSize = document.querySelector('#change-size')
+changeSize.addEventListener('click', getUserSize);
+
+function getUserSize() {
+    gridSize = prompt('What size grid would you like? Limit is 100.');
+    if (gridSize < 1 || gridSize > 100) {
+        alert('Please enter a number greater than 0 and less than 101.')
+        getUserSize();
+    }
+    resetGrid();
+}
+
+function resetGrid() {
+    const cells = document.querySelectorAll('#pixel')
+    cells.forEach((cell) => {
+        cell.remove()
     })
+    makeGrid(gridSize)
+}
